a8d11a27c44862591deecabe1ea9c26c9b598107
Petter Tchirkov <tchirkov.petter@gmail.com>
2022-04-14 22:14:23 +0300

    initial2

--- main.js
+++ main.js
@@ -61,3 +61,20 @@ let submitForm = (event) => {
 
 const sendForm = document.querySelector(".send");
 sendForm.addEventListener("click", submitForm);
+
+const countdownData = new Date("Apr 20, 2022 00:00:00").getTime();
+
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let distance = countdownData - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("mins").innerHTML = minutes;
+  document.getElementById("secs").innerHTML = seconds;
+}, 1000);
